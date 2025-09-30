38c3d3dae264f5a9cd05be0a8fd61b03ebeb89a4
aledelgadoo <alejandrodelgadovalera@gmail.com>
2025-09-30 15:37:13 +0100

    Filled the class Address.java with all the attributes.

--- src/org/ulpgc/is1/model/Address.java
+++ src/org/ulpgc/is1/model/Address.java
@@ -1,5 +1,40 @@
 package org.ulpgc.is1.model;
 
 public class Address {
+    private String street;
+    private int number;
+    private int floor;
+    private String city;
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
 }

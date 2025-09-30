8149b1999c72292f0d05fb0b97c31814c3f3f702
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2025-09-30 19:39:19 +0100

    fix: :bug: Deleted unnecesary class Member

--- src/org/ulpgc/is1/control/Main.java
+++ src/org/ulpgc/is1/control/Main.java
@@ -20,10 +20,6 @@ public class Main {
         System.out.println("Número de contactos en la agenda: " + agenda.contactCount());
         // 5. Imprimir por pantalla el número de contactos del grupo “trabajo”.
         System.out.println("Número de contactos en el grupo 'trabajo': " + trabajoGroup.contactCount());
-        // Imprimir la fecha de alta del miembro en el grupo
-        Member member = trabajoGroup.getMember(0);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        System.out.println("Fecha de alta en el grupo: " + member.getDate().format(formatter));
         // 6. Borrar el primer contacto del grupo “trabajo".
         trabajoGroup.removeContact(0);
         // 7. Volver a imprimir por pantalla el número de contactos del grupo “trabajo”.

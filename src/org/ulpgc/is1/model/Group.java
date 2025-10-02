0c8c20a9e0682684f813cf991e3487ccbab94032
TemiArtemi <ts.suarez2006@gmail.com>
2025-10-02 19:02:45 +0100

    Implementation class group

--- src/org/ulpgc/is1/model/Group.java
+++ src/org/ulpgc/is1/model/Group.java
@@ -1,5 +1,32 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+
+    private final String name;
+    private List<Contact> contacts;
+
+    public Group(String name) {
+        this.name = name;
+        this.contacts = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public removeContact(Contact contact) {
+        contacts.remove(contact);
+    }
 
 }

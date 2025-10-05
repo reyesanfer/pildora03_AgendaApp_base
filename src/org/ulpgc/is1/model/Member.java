28b1e5f233e68ab73cb4d77f846a1b835ebaf9e8
TemiArtemi <ts.suarez2006@gmail.com>
2025-10-05 18:14:33 +0200

    EXERCISE 1 PART 2: implementation of class Member but not the changes it involves in other classes

--- src/org/ulpgc/is1/model/Member.java
+++ src/org/ulpgc/is1/model/Member.java
@@ -1,5 +1,24 @@
 package org.ulpgc.is1.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Member {
+
+    private Contact contact;
+    private LocalDateTime date;
+
+    public Member(Contact contact, LocalDateTime date) {
+        this.contact = contact;
+        this.date = date;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
 
 }
